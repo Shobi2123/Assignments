54df6f4d249a22f7a7de4f7c43bc797935c060c0
Shobi2123 <142722313+Shobi2123@users.noreply.github.com>
2023-09-04 10:43:07 -0400

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,14 @@
+package org.college;
+
+public class College {
+  public void collegeName() {
+	  System.out.println("Sastra University");
+  }
+  public void collegeCode() {
+	  System.out.println("The code of my class is AA301");
+  }
+  
+  public void collegeRank() {
+	  System.out.println("The Rank of my college is 10");
+  }
+}
