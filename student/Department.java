98a0abbfc5f71def09080f225018a7ddcf11ac0c
Shobi2123 <142722313+Shobi2123@users.noreply.github.com>
2023-09-04 10:50:58 -0400

    Department.java

--- student/Department.java
+++ student/Department.java
@@ -7,12 +7,4 @@ public class Department extends College{
 	public void deptName() {
 		System.out.println("The Department name is Information Technology");
 	}
-/*public static void main(String[] args) {
-	Department dep=new Department();
-	dep.collegeName();
-	dep.collegeCode();
-	dep.collegeRank();
-	dep.deptName();
-	
-}*/
 }
